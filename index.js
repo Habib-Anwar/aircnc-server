1ba516b600fb14c1aeef120f67a3ac39b5414598
Habib-Anwar <121849760+Habib-Anwar@users.noreply.github.com>
2023-09-05 21:24:23 +0600

    user role

--- index.js
+++ index.js
@@ -43,6 +43,15 @@ async function run() {
         res.send(result)
     })
 
+    // Get User Role
+    app.get('/users/:email', async (req, res) =>{
+      const email = req.params.email
+      const query = { email: email }
+      const result = await usersCollection.findOne(query)
+      console.log(result)
+      res.send(result)
+    })
+
     // Get all rooms
     app.get('/rooms', async (req, res) =>{
       const result = await roomsCollection.find().toArray()
