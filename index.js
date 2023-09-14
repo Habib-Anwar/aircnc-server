9681fc5d474ed41be6d551dfcbc5563163e4155b
Habib-Anwar <121849760+Habib-Anwar@users.noreply.github.com>
2023-09-15 03:04:39 +0600

    token generation

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan')
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -31,6 +32,16 @@ async function run() {
     const roomsCollection = client.db('aircncDb').collection('rooms')
     const bookingsCollection = client.db('aircncDb').collection('bookings')
     
+    
+    // Generate jwt token
+    app.post('/jwt', (req, res) =>{
+      const email = req.body
+      const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET,
+       {expiresIn: '7d'})
+      console.log(token)
+      res.send({token})
+    })
+
     // Save user email and role in DB
     app.put('/users/:email', async (req, res) => {
         const email = req.params.email;
